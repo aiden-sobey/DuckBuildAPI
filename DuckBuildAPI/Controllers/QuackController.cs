57a8d1bb85c1cbbbb61ee110642114f5ad2a2a68
Aiden Sobey <sobey.aiden@gmail.com>
2019-10-30 14:01:55 +0800

    Quackify
    Weather forecasting is lame

---
+++ DuckBuildAPI/Controllers/QuackController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DuckBuildAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QuackController : ControllerBase
+    {
+        [HttpGet]
+        public bool ShouldIQuack(Guid duckId)
+        {
+            return true;
+        }
+
+        [HttpPost]
+        public Quack ScheduleQuack(Quack request)
+        {
+            // Create a quack
+            return new Quack();
+        }
+    }
+}
---
+++ DuckBuildAPI/Quack.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace DuckBuildAPI
+{
+    public class Quack
+    {
+        public DateTime Date { get; }
+
+        public Guid DuckId { get; }
+
+        public bool QuackStatus { get; set; }
+    }
+}
